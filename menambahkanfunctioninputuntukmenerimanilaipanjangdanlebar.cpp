89fe5897c0915c815a5d6c0a69cf056be28327ee
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-14 10:40:13 +0700

    menambahkanfunctioninputuntukmenerimanilaipanjangdanlebar.cpp

---
+++ menambahkanfunctioninputuntukmenerimanilaipanjangdanlebar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "Masukkan Panjang : ";
+    cin >> panjang;
+
+    cout << "Masukkan Lebar : ";
+    cin >> lebar;
+}
