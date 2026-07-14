3c44f725b07f1a4b03c8f8353554bd4911a36deb
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-14 10:32:40 +0700

    programutama.cpp

---
+++ programutama.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std; 
+
+float HitungRerataBerparameter(float x, float y)
+{
+    return (x + y) / 2;
+}
+
+string CekStatusBerparameter(float a)
+{
+    if (a >= 60)
+        return "Lulus";
+    else
+        return "Tidak Lulus";
+}
+
+int main()
+{
+    float Ujian1, Ujian2;
+
+    cout << "Masukkan Nilai Ujian 1 : ";
+    cin >> Ujian1;
+
+    cout << "Masukkan Nilai Ujian 2 : ";
+    cin >> Ujian2;
+
+    cout << "Status Kelulusan : "
+         << CekStatusBerparameter(
+                HitungRerataBerparameter(Ujian1, Ujian2));
+
+    return 0;
+}
